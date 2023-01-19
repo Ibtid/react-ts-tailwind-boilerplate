fdc8e674195ce1b591325ef241d6da86c31094b3
Ibtid <ibtid011@example.com>
2023-01-19 22:13:03 +0600

    first commit

---
+++ src/paths/apiPaths.ts
@@ -0,0 +1,13 @@
+const ApiPaths = {
+  Auth: {
+    Login: '/auth/login',
+    Refresh: '/auth/refresh',
+    Profile: '/auth/profile',
+    Forget: '/auth/forgotPassword',
+    Reset: '/auth/resetPassword',
+    Change: '/auth/changePassword',
+    Logout: '/auth/logout',
+  },
+};
+
+export default ApiPaths;
---
+++ src/paths/uiPaths.ts
@@ -0,0 +1,23 @@
+const UiPaths = {
+  Dashboard: '/dashboard',
+  Clinics: '/clinics',
+  ClinicDetails: '/clinic-details',
+  ClinicUserProfile: '/clinic-user-profile',
+  Diagnostics: '/diagnostics',
+  Finance: '/finance',
+  Investigations: '/investigations',
+  Invoices: '/invoices',
+  // AddInvoice: () => `${UiPaths.Invoices}/add`,
+  Patients: '/patients',
+  PatientReports: '/patient-reports',
+  SmsManagement: '/sms-management',
+  Companies: '/companies',
+  CompanyUsers: '/company-users',
+  Login: '/login',
+  ForgetPassword: '/forget-password',
+  ResetPassword: '/reset-password',
+  Profile: '/profile',
+  NotFound: '/notFound',
+};
+
+export default UiPaths;
